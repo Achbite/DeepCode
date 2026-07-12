f3db480c1900fab8e78f275056829266fb0bc042
Achbite <2726334183@qq.com>
2026-07-12 21:00:18 +0800

    fix(build): make package publication transactional

--- shells/deepcode-gui/src-tauri/build.rs
+++ shells/deepcode-gui/src-tauri/build.rs
@@ -1,5 +1,4 @@
 fn main() {
-    println!("cargo:rerun-if-changed=../dist");
     println!("cargo:rerun-if-changed=capabilities");
     tauri_build::try_build(
         tauri_build::Attributes::new().capabilities_path_pattern("./capabilities/*.json"),
